cac39399fb3a93a0af36909d97fd4913c7c60273
Adrian Sorroche <asorrocheL@protonmail.com>
2022-02-11 13:41:30 +0100

    implementacion test numeroAlumnos

--- src/com/iescomercio/ed/bloque2/repaso/modelo/CursoTest.java
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/CursoTest.java
@@ -63,7 +63,13 @@ class CursoTest {
 
 	@Test
 	void testNumeroAlumnos() {
-		fail("Not yet implemented");
+		Curso c = new Curso();
+		c.aniadirAlumno(new Persona("123456789","Pedro","Casas"));
+		int numAlumnos = c.numeroAlumnos();
+		
+		assertEquals(1,numAlumnos);
+		
+		
 	}
 
 	@Test
