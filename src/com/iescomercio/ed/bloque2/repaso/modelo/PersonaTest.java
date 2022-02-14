66640eb410d3594f7ccd1fb80af41a1e0d764209
Adrian Sorroche <asorrocheL@protonmail.com>
2022-02-14 11:21:30 +0100

    implementacion test Persona

---
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/PersonaTest.java
@@ -0,0 +1,45 @@
+package com.iescomercio.ed.bloque2.repaso.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonaTest {
+
+	@Test
+	void testPersonaStringStringString() {
+		Persona p1 = new Persona ("12345678A", "Adrian", "Sorroche");
+		Persona p2 = new Persona ("87457477L", "Pablo", "Gil");
+	}
+
+	@Test
+	void testGetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetApellido1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetApellido1() {
+		fail("Not yet implemented");
+	}
+
+}
